b88cf138b8819c57aeb3ac489edc9d203c9f72ea
blazros <blazros78@gmail.com>
2023-03-02 14:36:02 -0800

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char alphabet[26] = "zyxwvutsrqpomlkjihgfedcba";
+for (i = 0; i < 26; i++)
+{
+putchar(alphabet[i]);
+}
+putchar('\n');
+return (0);
+}
