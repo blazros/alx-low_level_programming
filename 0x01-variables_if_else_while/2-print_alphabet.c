3c8880011e09a48a851b2f7acd159db273e321d7
blazros <blazros78@gmail.com>
2023-03-02 13:02:41 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+for (int i = 0; i < 26; i++)
+{
+putchar(alphabet[i]);
+}
+putchar('\n');
+return (0);
+}
