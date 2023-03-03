fb5427768f5ecd9e027577710981004a205aab9b
blazros <blazros78@gmail.com>
2023-03-02 17:16:57 -0800

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,26 +7,26 @@
 int main(void)
 {
 int i, j, k;
-for (i = '0'; i < '8'; i++)
-{
-        for (j = i + 1; j < '9'; j++)
-        {
-		for (k = j + 1; k <= '9'; k++)
+	for (i = '0'; i < '8'; i++)
+	{
+		for (j = i + 1; j < '9'; j++)
 		{
-			if (i != j && i != k && j != k)
-                	{
-                        putchar(i);
-                        putchar(j);
-			putchar(k);
-                        if (i < '7' && j < '9' && k <= '9')
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                	}
+			for (k = j + 1; k <= '9'; k++)
+			{
+				if (i != j && i != k && j != k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i < '7' && j < '9' && k <= '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
 	}
-}
 putchar('\n');
 return (0);
 }
