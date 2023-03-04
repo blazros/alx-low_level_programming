272fbd16c5c2d6292531150602b5dd9194e7a6a2
blazros <blazros78@gmail.com>
2023-03-03 16:45:12 -0800

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _islower - Checks for lowercase character
+ * _isalpha - Checks for alphabetic character
  * @c: The character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
