c2d4078337f959a458d1e4c7d0e5642b567b154b
blazros <blazros78@gmail.com>
2023-03-04 17:48:41 -0800

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,27 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 void jack_bauer(void)
 {
-	int h, m;
-	for (h = '0'; h < 24; h++)
+int i, j, k;
+char hour1[] = {'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'
+, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '2', '2', '2', '2'};
+char hour2[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3'};
+	for (i = 0; i < 24; i++)
 	{
-		for (m = '0'; m < 60; m++)
+		for (j = 0; j <= 5; j++)
 		{
-			_putchar(h);
-			_putchar(':');
-			_putchar(m);
-			_putchar('\n');
+			for (k = 0; k <= 9; k++)
+			{
+				_putchar(hour1[i]);
+				_putchar(hour2[i]);
+				_putchar(58);
+				_putchar(j + '0');
+				_putchar(k + '0');
+				_putchar('\n');
+			}
 		}
 	}
 }
