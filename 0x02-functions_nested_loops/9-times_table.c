0b76f908f16670fe736606a8068f131c5974b940
blazros <blazros78@gmail.com>
2023-03-05 13:02:17 -0800

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,34 +7,34 @@
 void times_table(void)
 {
 	int a, b, c, x, y;
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			c = a * b;
-			if (c > 9)
-			{
-				y = c % 10 ;
-				x = (c - y) / 10;
-				if (b != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-				}
-				_putchar(x + '0');
-				_putchar(y + '0');
-			}
-			else
-			{
-				if (b != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
-				_putchar(c + '0');
-			}
-		}
-		_putchar('\n');
-	}
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (c > 9)
+{
+y = c % 10;
+x = (c - y) / 10;
+if (b != 0)
+{
+_putchar(44);
+_putchar(32);
+}
+_putchar(x + '0');
+_putchar(y + '0');
+}
+else
+{
+if (b != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(c + '0');
+}
+}
+_putchar('\n');
+}
 }
