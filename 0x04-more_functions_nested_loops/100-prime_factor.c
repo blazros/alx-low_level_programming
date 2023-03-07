e4ee12c2fdc64a63bd752575b3b8608be104de2a
blazros <blazros78@gmail.com>
2023-03-07 04:46:35 -0800

    11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,34 +7,21 @@
  */
 
 int main(void)
+
 {
-int i;
-long n = 612852475143;
-long maxPrime = -1;
-while (n % 2 == 0)
-{
-maxPrime = 2;
-n /= 2;
-}
-while (n % 3 == 0)
-{
-maxPrime = 3;
-n = n / 3;
-}
-for (i = 5; i <= sqrt(n); i += 6)
-{
-while (n % i == 0)
-{
-maxPrime = i;
-n = n / i;
-}
-while (n % (i + 2) == 0)
-{
-maxPrime = i + 2;
-n = n / (i + 2);
-}
-}
-if (n > 4)
-maxPrime = n;
-return (maxPrime);
+
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+	if (number % x == 0)
+	{
+	maxf = number / x;
+	}
+	}
+	printf("%ld\n", maxf);
+	return (0);
+
 }
