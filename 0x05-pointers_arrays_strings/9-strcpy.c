4dd51e1996a1105b3fd4ed9101bcedf8477222fa
blazros <blazros78@gmail.com>
2023-03-14 12:01:11 -0700

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (;src[i] != '\0'; i++)
+	while (;src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-dest[i + 1] = '\0';
+dest[i++] = '\0';
 return (dest);
 }
