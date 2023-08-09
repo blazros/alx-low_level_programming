aee271053ea2c812cb92a10830fc7db1496ea6b9
ahmedos1999 <ah_os5@yahoo.com>
2023-08-09 23:40:09 +0200

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
+ * read_textfile - Fnc that reads txt file and prints it to the STND_OUT
+ * @filename: The source file
+ * @letters: Number of letters to reads and prints
  *
- *
+ * Return: 0 on fail , readed characters on success.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -18,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1 )
+	if (fd == -1)
 		return (0);
 
 	readed = read(fd, buff, letters);
