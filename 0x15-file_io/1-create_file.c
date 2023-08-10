d888417e64595410ed9197879be1267dfd38cfd5
ahmedos1999 <ah_os5@yahoo.com>
2023-08-11 01:02:04 +0200

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,17 +10,18 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len;
+
 	len = _strlen(text_content);
-	
 	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
+
 	if (text_content)
 	{
-		write (fd, text_content, len);
+		write(fd, text_content, len);
 	}
 	close(fd);
 	return (1);
@@ -39,6 +40,5 @@ int _strlen(char *s)
 
 	while (s[c])
 		c++;
-	
 return (c);
 }
